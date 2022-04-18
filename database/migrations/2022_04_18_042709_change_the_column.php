28534a23f12ffb18a77ad8cfffa821379233bdf1
ViniciusLucasM <viniciuslucas@edu.unifil.br>
2022-04-18 19:11:51 -0300

    'Feito'

---
+++ database/migrations/2022_04_18_042709_change_the_column.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeTheColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(){
+        Schema::table('pets', function (Blueprint $table) {
+            $affected = DB::table('pets')
+                ->where('specie','bunny')
+                ->update(['specie'=>'coelho']);
+            $affected = DB::table('pets')
+                ->whereIn('specie',['mamba','mamba-negra'])
+                ->update(['specie'=>'cobra']);
+            $affected = DB::table('pets')
+                ->whereIn('specie',['pikachu','bulbasauro','squirtle'])
+                ->update(['specie'=>'pokemon']);
+            $affected = DB::table('pets')
+                ->whereIn('specie',['dragao de komodo','dragão de komodo'])
+                ->update(['specie'=>'dragao-de-komodo']);
+            $affected = DB::table('pets')
+                ->whereIn('specie',['papagaio','periquito'])
+                ->update(['specie'=>'ave']);
+            $affected = DB::table('pets')
+                ->where('specie','dog')
+                ->update(['specie'=>'cachorro']);
+            $affected = DB::table('pets')
+                ->whereIn('size',['SM','small'])
+                ->update(['size'=>'sm']);
+            $affected = DB::table('pets')
+                ->whereIn('size',['large','L'])
+                ->update(['size'=>'l']);
+            $affected = DB::table('pets')
+                ->whereIn('size',['medium','M'])
+                ->update(['size'=>'m']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('change_the_column');
+    }
+}
